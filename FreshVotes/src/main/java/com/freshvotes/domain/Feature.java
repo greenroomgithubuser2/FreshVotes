b86e1e6fdbb3f5238a1803d4139e67d0c7bad534
Mark M <tafmis@gmail.com>
2024-04-20 14:03:08 +0100

    Adding the vote and Comment join tables and creating the Features and Users tables and associated classes

---
+++ FreshVotes/src/main/java/com/freshvotes/domain/Feature.java
@@ -0,0 +1,46 @@
+package com.freshvotes.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="feature")
+public class Feature 
+{
+	private int id;
+	private String title;
+	private String description;
+	private String status;
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
